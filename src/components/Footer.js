46e4ce4ece699de8f86b78b7add9dcf73b258cb3
Felipe Borges <fcborges_@hotmail.com>
2021-05-18 12:37:14 -0300

    Navbar e Footer atualizados

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../contexts/authContext";
 import { useContext } from "react";
+import "../assets/styles/index.css"
 
 import ironhack from "../components/ironhack-logo.png";
 import Github from "../components/Github_logo-removebg-preview.png";
@@ -13,68 +14,95 @@ export default function Footer() {
   return (
     <footer
       className="navbar navbar-static-bottom navbar-light justify-content-center"
-      style={{ backgroundColor: "#1b1a17" }}
+      style={{ backgroundColor: "#3dadff" }}
     >
-      <div>
-        <div className="d-flex align-items-center">
-          <h4 style={{ color: "#F0E3CA" }}>Contact us:</h4>
-
-          <div>
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              className="navbar-brand"
-              href="https://github.com/FelipeBorges1991"
-            >
-              <img
-                src={Github}
-                width="50"
-                height="50"
-                className="d-inline-block align-top"
-                alt="logo img"
-              />
-            </a>
-            <h6 style={{ color: "#FF8303" }}>Felipe Borges</h6>
-          </div>
-
-          <div>
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              className="navbar-brand"
-              href="https://github.com/jeanclg"
-            >
-              <img
-                src={Github}
-                width="50"
-                height="50"
-                className="d-inline-block align-top"
-                alt="logo Github"
-              />
-            </a>
-            <h6 style={{ color: "#FF8303", paddingRight: "15px" }}>
-              Jean Garcia
-            </h6>
-          </div>
-
-          <div>
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              className="navbar-brand"
-              href="https://github.com/rmozer13811"
-            >
-              <img
-                src={Github}
-                width="50"
-                height="50"
-                className="d-inline-block align-top"
-                alt="logo img"
-              />
-            </a>
-            <h6 style={{ color: "#FF8303" }}>Richard Mozer</h6>
-          </div>
+    <div>
+          <h4 style={{ color: "#000e44" }}>Contact us:</h4>
         </div>
+
+      <div className="d-flex align-content-start align-items-center justify-content-start">
+
+        <div>
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            className="navbar-brand"
+            href="https://github.com/FelipeBorges1991"
+          >
+            <img
+              src={Github}
+              width="50"
+              height="50"
+              className="d-inline-block align-top"
+              alt="logo img"
+            />
+          </a>
+          <h6 style={{ color: "#000e44" }}>Felipe Borges</h6>
+        </div>
+
+        <div>
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            className="navbar-brand"
+            href="https://github.com/jeanclg"
+          >
+            <img
+              src={Github}
+              width="50"
+              height="50"
+              className="d-inline-block align-top"
+              alt="logo Github"
+            />
+          </a>
+          <h6 style={{ color: "#000e44" }}>Jean Garcia</h6>
+        </div>
+
+        <div>
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            className="navbar-brand"
+            href="https://github.com/rmozer13811"
+          >
+            <img
+              src={Github}
+              width="50"
+              height="50"
+              className="d-inline-block align-top"
+              alt="logo img"
+            />
+          </a>
+          <h6 style={{ color: "#000e44" }}>Richard Mozer</h6>
+        </div>
+      </div>
+
+      <div className="d-flex flex-column align-content-end">
+        <div className="d-flex flex-column">
+          <h6 className="text-center container" style={{ color: "#000e44" }}>
+            Created as a Fullstack project at Ironhack's Bootcamp campus São
+            Paulo in May/2021.
+          </h6>
+
+          <h6 className="text-center" style={{ color: "#000e44" }}>
+            Using MERN, Bootstrap and other libraries.
+          </h6>
+        </div>
+
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          className="navbar-brand d-flex justify-content-center"
+          href="https://www.ironhack.com/br/sao-paulo"
+        >
+          <img
+            src={ironhack}
+            width="90"
+            height="90"
+            className="d-inline-block align-top"
+            alt="logo Ironhack"
+          />
+        </a>
       </div>
     </footer>
   );
