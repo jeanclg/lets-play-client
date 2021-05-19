09fbd83bd1de1492e159e062b7f2aca999fc9454
Felipe Borges <fcborges_@hotmail.com>
2021-05-19 15:34:12 -0300

    logo atualizada / CSS Finalizado

--- src/routeComponents/Home.js
+++ src/routeComponents/Home.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Logo from "../images/logo_lets_play-removebg-preview.png";
+import Logo from "../images/Letsplayhome2.png";
 
 function Home() {
   return (
